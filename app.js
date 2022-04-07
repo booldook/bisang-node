1065eb771bfc8d4429c753a9ab4e8491be128adf
booldook <booldook@gmail.com>
2022-04-07 17:28:13 +0900

    ejs 설명

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+/* require global */
 const express = require('express');
 const app = express();
 const path = require('path');
 
+/* require router */
 const boardRouter = require('./routes/board');
 
+/* view init */
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+/* static router */
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+/* page router */
 app.get('/', (req, res, next) => {
 	res.send('<h1>HOME</h1>');
 });
 
 app.use('/board', boardRouter);
 
+/* server init */
 app.listen(3000, () => console.log('Server Running : http://127.0.0.1:3000'));
